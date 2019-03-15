864ed1c4f2747bccb858ce4ab8e1b34c397308ba
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2019-03-15 18:32:54 +0300

    Delete usefulfunc.h

--- usefulfunc.h
+++
@@ -1,9 +0,0 @@
-#ifndef USEFULFUNC
-#define USEFULFUNC
-
-int min(int *array,int count);
-int max(int *array,int count);
-void invert_array(int* array,const int count);
-void swap(int *a,int *b);
-
-#endif
