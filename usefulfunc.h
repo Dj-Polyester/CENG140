d69eb99d6ab32d41cd7fabc15811216bc21c9546
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2019-05-18 01:13:04 +0300

    Add files via upload

---
+++ usefulfunc.h
@@ -0,0 +1,19 @@
+#ifndef USEFULFUNC
+#define USEFULFUNC
+
+char* itos(int a);
+int charindex(char* array,char ch);
+int gcd(int a,int b);
+int lcm(int a,int b);
+int isprime(int num);
+int kbhit();
+int kbwait(char k);
+int min(int *array,int count);
+int max(int *array,int count);
+void invert_array(int* array,const int count);
+void swap(int *a,int *b);
+char* getIn();
+int len(char* array);
+int nmax(int *array,int count);
+
+#endif
