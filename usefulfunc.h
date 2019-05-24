078cb5f46809adcf5a86f5eb293f79c03e23843a
Dj-Polyester <45545087+Dj-Polyester@users.noreply.github.com>
2019-05-24 16:59:48 +0300

    Add files via upload

---
+++ usefulfunc.h
@@ -0,0 +1,21 @@
+#ifndef USEFULFUNC
+#define USEFULFUNC
+
+char* gen_rand_name(int len,int count,...);
+char *name_file(char *filename);
+char* itos(int a);
+int charindex(char* array,char ch);
+int gcd(int a,int b);
+int lcm(int a,int b);
+int isprime(int num);
+int kbhit();
+int kbwait(char k);
+int min(int *array,int count);
+int max(int *array,int count);
+void invert_array(int* array,const int count);
+void swap(int *a,int *b);
+char* getIn();
+int len(char* array);
+int nmax(int *array,int count);
+
+#endif
